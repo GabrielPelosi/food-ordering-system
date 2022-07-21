042a8876609c79411f6a94a792054f9f95066a56
GabrielPelosi <gabriel.pelosi.2020@gmail.com>
2022-07-21 22:00:55 +0100

    OrderEntity JPA classes

---
+++ order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/order/entity/OrderEntity.java
@@ -0,0 +1,52 @@
+package com.food.ordering.system.order.service.dataaccess.order.entity;
+
+
+import com.food.ordering.system.domain.valueObject.OrderStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+@Table(name = "orders")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderEntity {
+
+    @Id
+    private UUID id;
+    private UUID customerId;
+    private UUID restaurantId;
+    private UUID trackingId;
+    private BigDecimal price;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+    private String failureMessages;
+
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private OrderAddressEntity address;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItemEntity> items;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderEntity that = (OrderEntity) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
