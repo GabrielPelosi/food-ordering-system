dfd161766fa2d4ac1e89e14270bd05b9f14321e2
GabrielPelosi <gabriel.pelosi.2020@gmail.com>
2022-08-17 16:41:54 +0100

    Restaurant and Customer data access layer

--- order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/customer/mapper/CustomerDataAccessMapper.java
+++ order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/customer/mapper/CustomerDataAccessMapper.java
@@ -1,10 +1,15 @@
 package com.food.ordering.system.order.service.dataaccess.customer.mapper;
 
 
+import com.food.ordering.system.domain.valueObject.CustomerId;
+import com.food.ordering.system.order.service.dataaccess.customer.entity.CustomerEntity;
+import com.food.ordering.system.order.service.domain.entity.Customer;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CustomerDataAccessMapper {
 
-    public Customer c us
+    public Customer customerEntityToCustomer (CustomerEntity customerEntity){
+        return new Customer(new CustomerId(customerEntity.getId()));
+    }
 }
