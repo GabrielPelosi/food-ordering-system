316e2541dda4481f38365ca98b89d3a15b3a184e
GabrielPelosi <gabriel.pelosi.2020@gmail.com>
2022-08-29 21:50:32 +0100

    Payment Listener kafka messaging

---
+++ payment-service/payment-messaging/src/main/java/com/food/ordering/payment/service/messaging/listener/kafka/PaymentRequestKafkaListener.java
@@ -0,0 +1,42 @@
+package com.food.ordering.payment.service.messaging.listener.kafka;
+
+import com.food.ordering.payment.service.messaging.mapper.PaymentMessagingDataMapper;
+import com.food.ordering.system.kafka.consumer.KafkaConsumer;
+import com.food.ordering.system.kafka.order.avro.model.PaymentRequestAvroModel;
+import com.food.ordering.system.payment.service.domain.ports.input.message.listener.PaymentRequestMessageListener;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class PaymentRequestKafkaListener implements KafkaConsumer<PaymentRequestAvroModel> {
+
+    private final PaymentRequestMessageListener paymentRequestMessageListener;
+    private final PaymentMessagingDataMapper paymentMessagingDataMapper;
+
+    @Override
+    @KafkaListener(id = "${kafka-consumer-config.payment-consumer-group-id}",
+    topics = "${payment-service.payment-request-topic-name}")
+    public void receive(@Payload List<PaymentRequestAvroModel> messages,
+                        @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) List<String> keys,
+                        @Header(KafkaHeaders.RECEIVED_PARTITION_ID) List<Integer> partitions,
+                        @Header(KafkaHeaders.OFFSET) List<Long> offset) {
+
+        log.info("{} number of payment requests received with keys: {}, partitions: {} and offsets: {}",
+                messages.size(),
+                keys.toString(),
+                partitions.toString(),
+                offset.toString());
+
+
+    }
+}
